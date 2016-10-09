73a216980af3e8d58e297a1835316c8c3cbe601f
Alex Spinks <emperorlexx@hotmail.com>
2016-10-09 17:20:18 +0100

    Key the cache using ints instead of objects

--- src/main/java/ttftcuts/atg/generator/CoreNoise.java
+++ src/main/java/ttftcuts/atg/generator/CoreNoise.java
@@ -14,7 +14,7 @@ public class CoreNoise {
     }
 
     public NoiseCache.NoiseEntry getEntry(int x, int z) {
-        NoiseCache.CoordPair coord = new NoiseCache.CoordPair(x,z);
+        int coord = NoiseCache.coordHash(x,z);
         if (!this.cache.containsKey(coord)) {
             this.cache.put(coord, new NoiseCache.NoiseEntry(x,z));
         }
@@ -79,18 +79,18 @@ public class CoreNoise {
 
     //------ Cache class ---------------------------------------------------------
 
-    public static class NoiseCache extends LinkedHashMap<NoiseCache.CoordPair,NoiseCache.NoiseEntry> {
+    public static class NoiseCache extends LinkedHashMap<Integer,NoiseCache.NoiseEntry> {
 
         public NoiseEntry get(int x, int z) {
-            return this.get(new NoiseCache.CoordPair(x,z));
+            return this.get(coordHash(x,z));
         }
 
         public NoiseEntry put(int x, int z, NoiseEntry value) {
-            return this.put(new NoiseCache.CoordPair(x,z), value);
+            return this.put(coordHash(x,z), value);
         }
 
         public boolean containsKey(int x, int z) {
-            return this.containsKey(new NoiseCache.CoordPair(x,z));
+            return this.containsKey(coordHash(x,z));
         }
 
         @Override
@@ -98,35 +98,11 @@ public class CoreNoise {
             return this.size() > 256;
         }
 
-        // internals
-        public static class CoordPair {
-            final int x;
-            final int z;
-
-            public CoordPair(int x, int z) {
-                this.x = x;
-                this.z = z;
-            }
-
-            @Override
-            public int hashCode() {
-                int hash = 17;
-                hash = ((hash + x) << 5) - (hash + x);
-                hash = ((hash + z) << 5) - (hash + z);
-                return hash;
-            }
-
-            @Override
-            public boolean equals(Object other) {
-                if (this == other) {
-                    return true;
-                }
-                if (other instanceof CoordPair) {
-                    CoordPair oc = (CoordPair)other;
-                    return oc.x == this.x && oc.z == this.z;
-                }
-                return false;
-            }
+        public static int coordHash(int x, int z) {
+            int hash = 17;
+            hash = ((hash + x) << 5) - (hash + x);
+            hash = ((hash + z) << 5) - (hash + z);
+            return hash;
         }
 
         public static class NoiseEntry {
